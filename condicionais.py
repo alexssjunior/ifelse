e287016a9d93d1fd7e98b238d08cd63a0690392f
alexssjunior <alexsandrossjunior@gmail.com>
2024-11-25 19:45:03 -0300

    usodoelse

---
+++ condicionais.py
@@ -0,0 +1,7 @@
+#se for de maior ta liberado pra balada , caso seja de menor nao entra
+idade = int(input("informe a sua idade :"))
+
+if idade >=18:
+    print("permitido!")
+else:
+    print("bloqueado!")
